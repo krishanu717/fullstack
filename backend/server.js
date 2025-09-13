96272632a1598d92207eb60ea3e81c0891ddb526
Mondal <137494766+krishanu717@users.noreply.github.com>
2025-09-13 19:29:28 +0530

    update!!!!!!!!!!!!!

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Backend is running 🚀 on Railway!");
+  res.send("nigga Krishanu Deployed his ass!");
 });
 
 const PORT = process.env.PORT || 5000;
